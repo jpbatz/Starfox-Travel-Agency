790fa415be08dccdb8ab102f08347c150c7b975f
jpbatz <jph@hawaii.edu>
2014-11-23 12:25:51 -1000

    removed my start up, and replaced with attempt at Kelli's demo ideas

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+// gulp.task('styles', function () {
+//   return gulp.src('./sass/*.scss')
+//       .pipe(sass())
+//       .pipe(gulp.dest('./css'));
+// });
+
+// keeps gulp from crashing for scss errors
 gulp.task('styles', function () {
   return gulp.src('./sass/*.scss')
-      .pipe(sass())
+      .pipe(sass({
+         errLogToConsole: true
+      }))
       .pipe(gulp.dest('./css'));
 });
 
