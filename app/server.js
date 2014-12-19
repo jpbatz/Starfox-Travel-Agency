970bc50d7e600a0d4e061258b40f1a8487aa0b12
jpbatz <jph@hawaii.edu>
2014-12-18 19:41:49 -1000

    MBA jade set up and tested

---
+++ app/server.js
@@ -0,0 +1,31 @@
+/* INIT AND LIBRARIES */
+var express = require('express');
+var app = express();
+
+/* MIDDLEWARE */
+app.use(express.static('public'));
+// app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'jade');
+
+
+/* ROUTES */
+app.get('/', function(req, res) {
+
+  var locals = {
+    myname: "jon"
+  };
+
+  locals.mypets = [
+      "mochi",
+      "boba",
+      "cookie",
+      "cat"
+  ];
+
+  res.render('./index', locals);
+
+});
+
+
+/* EXPORT THIS FILE AS A MODULE */
+module.exports.app = app;
